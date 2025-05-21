0bbcc1dcbfd80b1a17f329c1b00f9df8a15bffba
AndreansxTech <koliberekart@icloud.com>
2025-05-21 20:48:30 +0200

    fixed missing fi after if

--- scripts/setup-ufw.sh
+++ scripts/setup-ufw.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Remember, This file needs to have execute permissions
+# Run this comand: chmod +x ./setup-ufw.sh
 # Run this script with sudo permissions: sudo ./setup-ufw.sh
 
 echo "Starting the configuration setup for UFW..."
@@ -8,7 +10,7 @@ then
   echo "UFW is not installed"
   apt-get update
   apt-get install -y ufw
-
+fi
 
 echo "Setting up default policies..."
 ufw default deny incoming
@@ -17,9 +19,10 @@ ufw default allow outgoing
 echo "Setting up specific rules..."
 ufw allow 80/tcp comment "Web server HTTP"
 ufw allow 443/tcp comment "Web server HTTPS"
-
-echo "Are you sure you want to enable UFW ? ( [Y]es/[N]o )" confirm_enable
+echo ""
+read -p "Are you sure you want to enable UFW ? ( [Y]es/[N]o )" confirm_enable
 if [[ "$confirm_enable" =~ ^[Yy]$ ]]
 then
   echo "Enabling UFW.."
   yes | ufw enable
+fi
