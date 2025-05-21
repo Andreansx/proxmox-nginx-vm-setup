85af8f5680fae0eec551745cf541c3f5eb713a88
AndreansxTech <koliberekart@icloud.com>
2025-05-21 21:33:29 +0200

    more functionality and colors

--- scripts/setup-ufw.sh
+++ scripts/setup-ufw.sh
@@ -5,11 +5,21 @@
 # Run this script with sudo permissions: sudo ./setup-ufw.sh
 
 echo "Starting the configuration setup for UFW..."
+
+ok="[  \e[1;32mOK\e[0m  ]"
+er="[  \e[1;31mERROR\e[0m  ]:"
+wr="[  \e[1;33mWARNING\e[0m  ]:"
 if ! command -v ufw &> /dev/null
 then
   echo "UFW is not installed"
   apt-get update
   apt-get install -y ufw
+  if [ $? -ne 0 ]
+  then
+    echo -e "$er Couldn't install UFW.. Aborting"
+    exit 1
+  fi
+  echo -e "$ok Sucessfulyl insttalled UFW\n"
 fi
 
 echo "Setting up default policies..."
@@ -18,11 +28,33 @@ ufw default allow outgoing
 
 echo "Setting up specific rules..."
 ufw allow 80/tcp comment "Web server HTTP"
+if [ $? -ne 0 ]
+then
+  echo -e "$er Couldn't setup HTTP rule"
+  exit 1
+fi
+
 ufw allow 443/tcp comment "Web server HTTPS"
+
 echo ""
+
 read -p "Are you sure you want to enable UFW ? ( [Y]es/[N]o )" confirm_enable
 if [[ "$confirm_enable" =~ ^[Yy]$ ]]
 then
   echo "Enabling UFW.."
   yes | ufw enable
+  if [ $? -ne 0 ]
+  then
+    echo -e "$er Couldn't enable UFW\n"
+    exit 1
+  fi
+  echo -e "$ok UFW has been successfully enabled\n"
+else
+  echo -e "$wr UFW has not been enabled. Remember to enable it later"
 fi
+
+echo ""
+
+echo -e "$ok Showing current UFW status.."
+
+ufw status verbose
