fd6e37ac529d67e2aa080b4cfd78a12f3b41eca3
Andreansx <147649026+Andreansx@users.noreply.github.com>
2025-05-22 07:52:12 +0200

    Update setup-nginx.sh

--- scripts/setup-nginx.sh
+++ scripts/setup-nginx.sh
@@ -4,5 +4,13 @@
 # Apply the execute permission: sudo chmod +x ./setup-nginx.sh
 # Run with sudo priveleages
 
+ok="[  \e[1;32mOK\e[0m  ]"
 apt-get update
+if [ $? -ne 0 ]
+then
+  exit 1
+else
+  echo -e "$ok Sucessfully updated package list"
+fi
+echo ">>> Begining Nginx install"
 apt-get install nginx
