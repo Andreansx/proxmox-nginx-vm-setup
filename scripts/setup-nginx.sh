445f885eb4961f78d83cdc7314d9ee3de6aa69ca
Andreansx <147649026+Andreansx@users.noreply.github.com>
2025-05-22 07:46:21 +0200

    Create setup-nginx.sh

---
+++ scripts/setup-nginx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script installs and completes basic configuration for Nginx web server
+# Apply the execute permission: sudo chmod +x ./setup-nginx.sh
+# Run with sudo priveleages
+
+apt-get update
+apt-get install nginx
