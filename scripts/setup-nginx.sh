2d2267f68af769e8c828a27650cf70588bc4c011
Andreansx <147649026+Andreansx@users.noreply.github.com>
2025-05-22 08:13:37 +0200

    more things

--- scripts/setup-nginx.sh
+++ scripts/setup-nginx.sh
@@ -5,6 +5,7 @@
 # Run with sudo priveleages
 
 ok="[  \e[1;32mOK\e[0m  ]"
+er="[  \e[1;33mERROR\e[0m  ]:"
 apt-get update
 if [ $? -ne 0 ]
 then
@@ -17,6 +18,13 @@ echo "Checking for nginx existence.."
 if command ! -v nginx & /dev/null
 then
   echo ">>> Begining Nginx install"
-  apt-get install nginx
-  if [ $? -ne 0 ]; then; 
-  
+  apt-get install -y nginx
+  if [ $? -ne 0 ]; then;
+  echo -e "$er Couldn't install Nginx.. Aborting"
+  exit 1
+  else
+  echo -e "$ok Sucessfully installed Nginx\n"
+  fi
+else
+  echo -e "$ok Nginx already installed\e"
+fi
